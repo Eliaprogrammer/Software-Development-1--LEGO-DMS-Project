eb3d2166c9f4d7fa82359228f23a938e7844c50d
Elia Schwartz <eschwartz6@mail.valenciacollege.edu>
2025-10-14 16:01:12 -0400

    Updated the Menu.java
    Add the method declarations for addSetFromFile and addSetManually

---
+++ src/main/java/com/example/legodmsproject/Menu.java
@@ -0,0 +1,21 @@
+package com.example.legodmsproject;
+
+/**
+ * Elia Schwartz
+ * October 10, 2025,
+ * CEN 3024C 14877 Software Development
+ * Menu.java
+ * This file is an interface that only holds the method declarations
+ */
+public interface Menu {
+    String displayLegoMenu();
+    boolean addSetFromFile();
+    boolean addSetManually();
+    LegoSet removeSet();
+    LegoSet displaySet();
+    LegoSet updateSet();
+    double retrieveTotal();
+    boolean exit();
+
+
+}
